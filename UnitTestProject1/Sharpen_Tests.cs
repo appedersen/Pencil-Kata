ce3ea966a05f562b1dd56cbd4c548b2c48fb5cf6
andy <appedersen@jacks.sdstate.edu>
2018-01-31 21:06:43 -0600

    Added unit tests for sharpening a pencil

---
+++ UnitTestProject1/Sharpen_Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using Pillar_Pencil_Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Sharpen_Tests
+    {
+        [TestMethod]
+        public void Pencils_Can_Be_Sharpened()
+        {
+            
+            Pencil Pencil_instance = new Pencil(6,10);
+            Pencil_instance.Write("Hello ");
+            Pencil_instance.Sharpen();
+            Pencil_instance.Write("World");
+
+            Assert.AreEqual("Hello World", Pencil_instance.Read_Paper());
+        }
+
+        [TestMethod]
+        public void Stubby_Pencils_Cannot_Be_Sharpened()
+        {
+            Pencil Pencil_instance = new Pencil(6, 1);
+            Pencil_instance.Write("Hello ");
+            Pencil_instance.Sharpen();
+            Pencil_instance.Write("World ");
+            Pencil_instance.Sharpen();
+            Pencil_instance.Write("; ");
+
+            Assert.AreEqual("Hello World   ", Pencil_instance.Read_Paper());
+
+             
+        }
+}
