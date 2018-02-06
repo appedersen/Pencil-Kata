5e530a6902eafe3051a7937d813c196e8bf215c9
andy <appedersen@jacks.sdstate.edu>
2018-02-06 16:14:27 -0600

    Removed constructor overloads and boolean toggles for durability
    From this point on all pencil objects will be assumed to use both the pencil and eraser durability.
    
    All tests green

--- Pillar_Pencil_Kata/Pencil.cs
+++ Pillar_Pencil_Kata/Pencil.cs
@@ -16,53 +16,16 @@ namespace Pillar_Pencil_Kata
 
         int Index_of_Last_Erased_Segment;
 
-
-
-        bool Durability_enabled; //The requirements said pencils _can_ have durability
-                                 //so I am assuming that a pencil without a defined durability is
-                                 //indestructible. In a real case I'd have this clarified. 
-        bool Eraser_durability_enabled;
-
         int Pencil_Length;
-        public Pencil()
-        {
-            Paper = ""; //Brand new paper should be blank
-            Durability_enabled = false;
-            Eraser_durability_enabled = false;
-        }
 
-        public Pencil(int Initial_Durabiltiy) //Overload for constructor, provided an initial durability
-        {
-            Paper = "";
-            Pencil_Length = 1;
-            Durability_enabled = true; //this pencil can be dulled
-            Default_durability = Initial_Durabiltiy;
-            Current_durability = Initial_Durabiltiy;
-            Eraser_durability_enabled = false;
-            Index_of_Last_Erased_Segment = 0;
-        }
-
-        public Pencil(int Initial_Durabiltiy, int Initial_Length)  //Overload for constructor, provided an initial durability and length
-        {
-            Paper = "";
-            Durability_enabled = true; //this pencil can be dulled
-            Default_durability = Initial_Durabiltiy;
-            Current_durability = Initial_Durabiltiy;
-            Pencil_Length = Initial_Length;
-            Eraser_durability_enabled = false;
-            Index_of_Last_Erased_Segment = 0;
-        }
-
+       
         public Pencil(int Initial_Durabiltiy, int Initial_Length, int Eraser_Initial_Durability)  //Overload for constructor, provided an initial durability and length
         {
             Paper = "";
-            Durability_enabled = true; //this pencil can be dulled
             Default_durability = Initial_Durabiltiy;
             Current_durability = Initial_Durabiltiy;
             Pencil_Length = Initial_Length;
-
             Eraser_durability = Eraser_Initial_Durability;
-            Eraser_durability_enabled = true;
             Index_of_Last_Erased_Segment = 0;
 
         }
@@ -123,7 +86,7 @@ namespace Pillar_Pencil_Kata
 
             index_of_substring = Paper.LastIndexOf(Substring_to_be_erased);
 
-            if (Substring_to_be_erased.Length > Eraser_durability && Eraser_durability_enabled == true)
+            if (Substring_to_be_erased.Length > Eraser_durability)
             {
                 Undeleted_Character_Count = Get_Count_Of_Undeleted_Characters_From_Partial_Erase(Substring_to_be_erased);
             }
@@ -159,7 +122,7 @@ namespace Pillar_Pencil_Kata
 
         public void Sharpen()
         {
-            if(Durability_enabled && Pencil_Length > 1)
+            if(Pencil_Length > 1)
             {
                 Current_durability = Default_durability;
                 Pencil_Length--;
@@ -169,52 +132,41 @@ namespace Pillar_Pencil_Kata
 
         public void Write(string Intended_Message)
         {
-
-            if (Durability_enabled == false)
+            for(int Character_Postition = 0; Character_Postition < Intended_Message.Length; Character_Postition++)
             {
-                Paper += Intended_Message;
-            }
-            else
-            {
-                for(int Character_Postition = 0; Character_Postition < Intended_Message.Length; Character_Postition++)
+                if(char.IsUpper(Intended_Message[Character_Postition]))
                 {
-                    if(char.IsUpper(Intended_Message[Character_Postition]))
-                    {
-                        if(Current_durability >= 2)
-                        {
-                            Paper += Intended_Message[Character_Postition];
-                            Current_durability -= 2;
-                        }
-                        else
-                        {
-                            Paper += " ";
-                        }
-                    }
-
-                    else if(char.IsWhiteSpace(Intended_Message[Character_Postition]))
+                    if(Current_durability >= 2)
                     {
                         Paper += Intended_Message[Character_Postition];
+                        Current_durability -= 2;
                     }
-
                     else
                     {
-                        if (Current_durability >= 1)
-                        {
-                            Paper += Intended_Message[Character_Postition];
-                            Current_durability -= 1;
-                        }
-                        else
-                        {
-                            Paper += " ";
-                        }
+                        Paper += " ";
                     }
+                }
 
+                else if(char.IsWhiteSpace(Intended_Message[Character_Postition]))
+                {
+                    Paper += Intended_Message[Character_Postition];
+                }
 
+                else
+                {
+                    if (Current_durability >= 1)
+                    {
+                        Paper += Intended_Message[Character_Postition];
+                        Current_durability -= 1;
+                    }
+                    else
+                    {
+                        Paper += " ";
+                    }
                 }
 
 
             }
-            return;
         }
 
         public string Read_Paper()
